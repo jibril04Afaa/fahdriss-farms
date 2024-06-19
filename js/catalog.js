f9fdf5bf9a1be14993c95851551fe255d9bd77a7
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2024-06-19 15:08:36 +0000

    changes made

--- js/catalog.js
+++ js/catalog.js
@@ -6,65 +6,6 @@ let productDescs = document.querySelectorAll(".product-desc")
 let body = document.querySelector("body")
 let bags = document.querySelectorAll(".fa-bag-shopping")
 
-learnMoreBtns.forEach(learnMoreBtn => {
-    learnMoreBtn.addEventListener('click', function() {
-        // toggle product descriptions
-        productDescs.forEach(productDesc => {
-            if (productDesc.style.display === 'none') {
-                productDesc.style.display = 'block'
-            } else {
-                productDesc.style.display = 'none'
-                learnMoreBtn.textContent = 'Show Less'
-            }
-            
-        })
-    })
-    
-})
-
-
-// toggle red heart
-hearts.forEach(heart => {
-    heart.addEventListener('click', function() {
-        if (heart.style.color === '') {
-            heart.style.color = 'red'
-            setTimeout(() => {
-                alert("Added to favorites! ")
-            }, 100)
-            
-            // add selected item to favorites
-
-        } else {
-            heart.style.color = ''
-            setTimeout(() => {
-                alert("Removed from favorites ")
-            }, 100)
-            // remove selected item from favorites
-        }
-    })
-})
-
-
-
-
-// toggle cart item
-bags.forEach(bag => {
-    bag.addEventListener('click', function() {
-        
-        if (bag.style.color === 'black') {
-            bag.style.color = '#898E4C'
-            setTimeout(() => {
-                alert("Item added to bag")
-            }, 100)
-            
-        } else {
-            bag.style.color = 'black'
-            setTimeout(() => {
-                alert("Item removed from bag")
-            }, 100)
-        }
-    })
-})
 
 
 let productCardParent = document.querySelector(".products-container")
@@ -77,20 +18,9 @@ async function fetchJSONandPopulate() {
         const parsedData = await response.json()
         
         parsedData.forEach(data => {
-            let IDs = data.id
-            // let productName = data.name
-            // let prices = data.prices
-            // let images = data.image
-            // let categories = data.category
-            // let widths = data.width
-            // let heights = data.height
-
             // create new product card
             let productCard = document.createElement('div')
-
-            let icons = document.createElement('div')
-            let productImage = document.createElement('div')
-            let productInfo = document.createElement('div')
+            
 
             // dynamically populate data to the product cards
             productCard.innerHTML = 
@@ -132,6 +62,26 @@ async function fetchJSONandPopulate() {
 }
 
 fetchJSONandPopulate()
+
+// toggle red heart
+// productCardParent.addEventListener('click', function(event) {
+//     let heart = event.target
+//     if (heart.style.color != 'red') {
+//         heart.style.color = 'red'
+//     } else {
+//         heart.style.color = 'black'
+//     }
+// })
+
+
+// toggle cart item
+productCardParent.addEventListener('click', function(event) {
+    if (event.target.style.color != '#898E4C') {
+        event.target.style.color = '#898E4C'
+    } else {
+        event.target.style.color = 'black'
+    }
+})
 
 
 // code from stack overflow user Mervis Mascarenhas
