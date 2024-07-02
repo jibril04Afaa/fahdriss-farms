f3e9cd013588bbb0b0ec97392b59a41b68f95628
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2024-07-02 12:27:12 +0000

    setup a mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,46 @@
-const http = require("http")
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
 
-http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"})
+
+// database connection
+mongoose
+    .connect("mongodb://localhost:27017/shop_db")
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('Server connected on port 3000 with mongo db');
+        })
+    })
+    .catch((error) => console.log(error))
+
+
+app.get('/', (req, res) => {
+    console.log("Hello World")
+    res.send("Working ")
+})
+
+// mongoose schema
+const Schema = mongoose.Schema
+const productSchema = new Schema({
+    id: Number,
+    name: String,
+    prices: [String],
+    image: String,
+    category: String,
+    width: String,
+    height: String
+})
+
+// mongoose model
+const Product = mongoose.model('Product', productSchema)
+
+// fetch products
+app.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find()
+        res.json(products)
+    } catch(err) {
+        console.error(err)
+        res.status(500).json({errorMessage: "Error fetching products"})
+    }
 })
